f39edfda620dc32f033f00067c3974ed4827f969
joshuathomas3 <joshuathomas60@outlook.com>
2024-03-06 11:40:51 +0000

    Completed assessment objectives, runs successfully without errors.

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -8,16 +8,28 @@ namespace CMP1903_A1_2324
 {
     internal class Die
     {
-        /*
-         * The Die class should contain one property to hold the current die value,
-         * and one method that rolls the die, returns and integer and takes no parameters.
-         */
-
         //Property
+        private int _value;
 
-
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+            private set
+            {
+                _value = value;
+            }
+        }
+        
         //Method
-
+        public int Roll()
+        {
+            Random random = new Random();
+            _value = random.Next(1, 7);
+            return _value;
+        }
 
     }
 }
--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -1,21 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
     internal class Game
     {
-        /*
-         * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
-         *
-         * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
-         * rolls could be continous, and the totals and other statistics could be summarised for example.
-         */
+        public int[] MainGame()
+        {
+            var die = new Die();
+            int die1 = die.Roll();
+            Console.WriteLine("Roll: " + Convert.ToString(die1));
+            int die2 = die.Roll();
+            Console.WriteLine("Roll: " + Convert.ToString(die2));
+            int die3 = die.Roll();
+            Console.WriteLine("Roll: " + Convert.ToString(die3));
 
-        //Methods
+            int dieTotal = die1 + die2 + die3;
+            Console.WriteLine("Sum of die: " + Convert.ToString(dieTotal));
 
+            int[] numbers = { die1, die2, die3, dieTotal };
+            return numbers;
+
+        }
+        
     }
 }
